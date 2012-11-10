0dbf356b88419e8bd251308683d13ecc7a2d8232
Michał El <m.elfartas@gmail.com>
2012-11-10 13:35:26 +0100

    Pierwsza wersja projektu. Utworzone zostały podstawowe klasy do obsługi socket'ów.

---
+++ BaseProject/klasy.hpp
@@ -0,0 +1,225 @@
+#ifndef KLASY_HPP_INCLUDED
+#define KLASY_HPP_INCLUDED
+#include <stdio.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+#include <netdb.h>
+#include <time.h>
+#include <ctype.h>
+using namespace std;
+
+class Addr
+{
+    struct sockaddr_in addr;
+    socklen_t addrlen;
+
+public:
+
+    struct sockaddr_in *sockaddr_in_ptr;
+    struct sockaddr *sockaddr_ptr;
+    struct in_addr *in_addr_ptr;
+    in_addr_t *s_addr_ptr;
+    socklen_t *addrlen_ptr;
+
+    Addr()
+    {
+        sockaddr_in_ptr=(&addr);
+        sockaddr_ptr=(struct sockaddr*)(&addr);
+        in_addr_ptr=(&addr.sin_addr);
+        s_addr_ptr=(&addr.sin_addr.s_addr);
+        addrlen_ptr=(&addrlen);
+
+        addr.sin_family=AF_INET;
+        addrlen=sizeof(addr);
+    }
+
+    Addr(const Addr &copy)
+    {
+        addr=*copy.sockaddr_in_ptr;
+        addrlen=*copy.addrlen_ptr;
+
+        sockaddr_in_ptr=(&addr);
+        sockaddr_ptr=(struct sockaddr*)(&addr);
+        in_addr_ptr=(&addr.sin_addr);
+        s_addr_ptr=(&addr.sin_addr.s_addr);
+        addrlen_ptr=(&addrlen);
+    }
+
+    Addr& operator=(Addr &copy)
+    {
+        addr=*copy.sockaddr_in_ptr;
+        addrlen=*copy.addrlen_ptr;
+        return *this;
+    }
+
+    bool initByName(string name,int port)
+    {
+        struct hostent *host;
+        host=gethostbyname(name.c_str());
+        if(host==NULL)
+        {
+            return false;
+        }
+        addr.sin_addr=*((struct in_addr*)(host->h_addr));
+        addr.sin_port=htons(port);
+        return true;
+    }
+
+    bool initByIP(string ip,int port)
+    {
+        inet_pton(AF_INET, ip.c_str(), &addr.sin_addr);
+        addr.sin_port=htons(port);
+        return true;
+    }
+
+    bool initHost(int port)
+    {
+        addr.sin_addr.s_addr=INADDR_ANY;
+        addr.sin_port=htons(port);
+        return true;
+    }
+
+    int getPort()
+    {
+        return (int)(ntohs(addr.sin_port));
+    }
+
+    string getIP()
+    {
+        char buf[20];
+        string ip;
+        inet_ntop(AF_INET, &addr.sin_addr, buf, addrlen);
+        ip=buf;
+        return ip;
+    }
+
+};
+
+struct transmition_info
+{
+    int code;   ///kod weryfikacyjny
+
+    unsigned int comand;    ///komenda (rodzaj transmisji)
+    unsigned long long buf_size;  ///rozmiar bufora
+    unsigned long long data_size; ///rozmiar danych
+};
+
+class Conection
+{
+    protected:
+    int my_socket;
+    Addr addr;
+    bool active;
+
+    virtual bool init()
+    {
+        disconnect();
+        my_socket=socket(AF_INET,SOCK_STREAM,0);
+        if(my_socket<0)
+        {
+            return false;
+        }
+
+        int status;
+        status=connect(my_socket,addr.sockaddr_ptr,*addr.addrlen_ptr);
+        if(status<0)
+        {
+            close(my_socket);
+            return false;
+        }
+        active=true;
+        return true;
+    }
+
+public:
+
+    Conection()
+    {
+        active=false;
+    }
+
+    void disconnect()
+    {
+        if(active)
+        {
+            close(my_socket);
+            active=false;
+        }
+    }
+
+    bool initByAddr(Addr addr)
+    {
+        this->addr=addr;
+        return init();
+    }
+
+    void initByCopy(int existingSocket,Addr addr)
+    {
+        this->addr=addr;
+        my_socket=existingSocket;
+        active=true;
+    }
+
+    int getSocket()
+    {
+        return my_socket;
+    }
+
+    bool isActive()
+    {
+        return active;
+    }
+};
+
+class Host: public Conection
+{
+    protected:
+    virtual bool init()
+    {
+        disconnect();
+        my_socket=socket(AF_INET,SOCK_STREAM,0);
+        if(my_socket<0)
+        {
+            return false;
+        }
+
+        int status;
+        status=bind(my_socket,addr.sockaddr_ptr,*addr.addrlen_ptr);
+        if(status<0)
+        {
+            close(my_socket);
+            return false;
+        }
+
+        status=listen(my_socket,5);
+        if(status<0)
+        {
+            close(my_socket);
+            return false;
+        }
+
+        active=true;
+        return true;
+    }
+
+    public:
+    Conection waitForClient()
+    {
+        Addr new_addr;
+        Conection client;
+        int new_sock;
+        new_sock=accept(my_socket, new_addr.sockaddr_ptr, new_addr.addrlen_ptr);
+        if(new_sock>0)
+        {
+            client.initByCopy(new_sock,new_addr);
+        }
+        return client;
+    }
+};
+
+
+
+#endif // KLASY_HPP_INCLUDED
---
+++ BaseProject/main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <vector>
+#include <boost/thread.hpp>
+#include "klasy.hpp"
+using namespace std;
+using namespace boost;
+
+void new_client(int socket, string addr)
+{
+    cout << socket << " " << addr << endl;
+}
+
+void funkcja(Addr adr)
+{
+    cout << adr.getIP() << " : " << adr.getPort() << endl;
+}
+
+int main()
+{
+    thread t1(new_client,10,"net.com");
+    cout << "Hello world!" << endl;
+    t1.join();
+    return 0;
+}
