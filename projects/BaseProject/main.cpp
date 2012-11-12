7c6872ca3b437a7d37f6d74dbd4e70ac6bb1e45e
Michał El <m.elfartas@gmail.com>
2012-11-12 10:58:53 +0100

    Zmieniono część funkcji na globalne. Wersja jeszcze nie kompilowana, do przetestowania.

--- projects/BaseProject/main.cpp
+++ projects/BaseProject/main.cpp
@@ -3,37 +3,67 @@
 #include <boost/thread.hpp>
 #include "klasy.hpp"
 using namespace std;
-using namespace boost;
 
-void new_client(Connection klient)
+Host host;
+bool active=false;
+boost::thread_group clients;
+
+void initServerConection(int argc, char **argv)
 {
-    char buf[51];
-    buf[50]=0;
-    while(read(klient,buf,50))
+    if(argc<2)
     {
-        printf("%i: %s",(int)klient,buf);
-        for(int i=0;i<50;i++)
-        {
-            buf[i]=0;
-        }
+        printf("Invalid initialization, use: %s port\n",argv[0]);
+        exit(0);
     }
-    klient.disconnect();
-}
-
-int main()
-{
-
-
-    Host host;
-    if(!host.initByAddr(Addr(6668)))
+    int port=atoi(argv[1]);
+    if((port<=1024)||(port>=65535))
+    {
+        printf("Invalid port number, port should be greater than 1024 and lesser than 65535.\n");
+        exit(0);
+    }
+    if(!host.initByAddr(Addr(port)))
     {
         printf("Could not inialize host connection.\n");
         exit(0);
     }
+}
 
-    Server reader(host,new_client);
-    reader.start();
+void serverMainLoop(void (*client_handler)(Connection))
+{
+    active=true;
+    while(active and connection.isActive())
+    {
+        Connection newClient=connection.waitForClient();
+        clients.add_thread(new boost::thread(client_handler,newClient));
+    }
+}
 
+void consoleHandler()
+{
+    while(active)
+    {
+        char c;
+        cin >> c;
+        if(c=='q')
+        {
+            active=false;
+            //host.disconnect();
+        }
+    }
+}
+
+void new_client(Connection klient)
+{
+    write(klient,"Witam!\n",7);
+    klient.disconnect();
+}
+
+int main(int argc, char **argv)
+{
+    initServerConection(argc,argv);
+    boost::thread serwer(serverMainLoop,new_client);
+    consoleHandler();
+    serwer.join();
     host.disconnect();
     return 0;
 }
